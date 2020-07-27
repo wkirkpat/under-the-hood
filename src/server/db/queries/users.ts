858a23bf0d79150b27db6194f1aca7b7dd00ce06
Will <wk1394@gmail.com>
2020-07-27 14:55:28 -0500

    created queries

--- src/server/db/queries/users.ts
+++ src/server/db/queries/users.ts
@@ -1,8 +1,16 @@
 import { Query } from "../index";
 
-//gets all users from the database
+//gets all users
 export const getAllUsers = async () => Query("SELECT * FROM users");
 
+//gets a single user by id
+export const getUserById = async (id: string) => Query("SELECT users.username, users.firstName, users.lastName FROM users WHERE id = ?", [id]);
+
+//Gets a user by email, used for auth
+export const getUserByEmail = async (email: string) => Query("SELECT * FROM users WHERE email = ?", [email]); 
+
 export default {
-    getAllUsers
+    getAllUsers,
+    getUserById,
+    getUserByEmail
 }
