d3d21468d3a93422f70684af870667ef479ac7df
Will <wk1394@gmail.com>
2020-07-28 16:14:53 -0500

    fixed infinte loading issue

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,13 +1,11 @@
 import * as express from "express";
 import apiRouter from "./routes";
 import * as path from "path";
-import * as cors from "cors";
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(express.json());
-app.use(cors);
 app.use(apiRouter);
 
 //This is a fix for being unable to refresh pages. It basically reroutes the user to the index so that the react router can take over
