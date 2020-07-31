4784b32c7c2f76b77d8e028be4d119778e5614b0
Will <wk1394@gmail.com>
2020-07-31 12:16:46 -0500

    back end auth fixed

--- src/server/routes/api/index.ts
+++ src/server/routes/api/index.ts
@@ -1,8 +1,22 @@
 import * as express from "express";
 import vehiclesRouter from "./vehicles";
 import usersRouter from "./users";
+import * as passport from "passport";
 
 const router = express.Router();
+
+router.use((req, res, next) => {
+  passport.authenticate(
+    "bearer",
+    {
+      session: false,
+    },
+    (err, user, info) => {
+      if (user) req.user = user;
+      return next();
+    }
+  )(req, res, next);
+});
 
 router.use("/vehicles", vehiclesRouter);
 router.use("/users", usersRouter);
