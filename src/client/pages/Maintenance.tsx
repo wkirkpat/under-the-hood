442316edc5c290a1712ddfb369a0df6cfe5a8218
adrian35124 <adrian35124@gmail.com>
2020-08-05 15:23:38 -0500

    Working with Moment Js

--- src/client/pages/Maintenance.tsx
+++ src/client/pages/Maintenance.tsx
@@ -1,28 +1,33 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useLayoutEffect } from "react";
 import Header from "../components/Header";
 import { TextField } from "@material-ui/core";
 import { json } from "../utils/api";
-import moment from "moment";
+import moment, { months } from "moment";
 
 const Maintance: React.FC<IMaintenanceProps> = (props) => {
   const [carInfo, setCarInfo] = useState([]);
-  const [nextOilChange, setNextOilChange] = useState("");
+  // const [nextOilChange, setNextOilChange] = useState("");
 
+  const firstUpdate = useRef(true);
+  useLayoutEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+    // setNextOilChange(moment(carInfo[0].lastOilChange).add(6, 'months').calendar());
+  });
+  useEffect(() => {
+    getCarInfo();
+  }, []);
   let getCarInfo = async () => {
     try {
       let info = await json(`/api/vehicles/info/1`);
       setCarInfo(info);
-      setNextOilChange(carInfo[0].lastOilChange)
       // console.log(info);
     } catch (e) {
       throw e;
     }
   };
-  
-  useEffect(() => {
-    getCarInfo();
-    // console.log(moment().format('MM/DD/YYYY'));
-  }, []);
 
   return (
     <>
@@ -39,8 +44,8 @@ const Maintance: React.FC<IMaintenanceProps> = (props) => {
       <div className="carInformation">
         <h2 className="user">Will Kirkpatrick</h2>
         <p className="make">{carInfo[0]?.make}</p>
-        <p className="model">{carInfo[0] && carInfo[0].model}</p>
-        <p className="year">{carInfo[0] && carInfo[0]._year}</p>
+        <p className="model">{carInfo[0]?.model}</p>
+        <p className="year">{carInfo[0]?._year}</p>
       </div>
       <img
         src="https://www.vehiclehistory.com/evox_color_compressed/chevrolet/equinox/2006/3230/chevrolet-equinox-2006-001-3230-15U-768.jpg"
@@ -49,7 +54,7 @@ const Maintance: React.FC<IMaintenanceProps> = (props) => {
       ></img>
       <div className="previousOilChange mt-2">
         <h2 className="card-title">Previous Oil Change:</h2>
-        <p className="card-text">Oil Type:{carInfo[0] && carInfo[0].oilType}</p>
+        <p className="card-text">Oil Type:{carInfo[0]?.oilType}</p>
         <p className="card-text">Oil Filter:</p>
         <p className="card-text">
           Date:{carInfo[0] && carInfo[0].lastOilChange}
@@ -58,7 +63,7 @@ const Maintance: React.FC<IMaintenanceProps> = (props) => {
           id="datetime-local"
           label="Previous appointment"
           type="datetime-local"
-          defaultValue="2017-05-24T10:30"
+          defaultValue= {carInfo[0]?.lastOilChange} 
           className="container"
           InputLabelProps={{
             shrink: true,
@@ -67,9 +72,9 @@ const Maintance: React.FC<IMaintenanceProps> = (props) => {
       </div>
       <div className="nextOilChange mt-2">
         <h2 className="card-title">Next Oil Change:</h2>
-        <p className="card-text">Oil Type:</p>
+        <p className="card-text">Oil Type: {carInfo[0]?.oilType}</p>
         <p className="card-text">Oil Filter:</p>
-        <p className="card-text">Date:</p>
+        <p className="card-text">Date: {moment(carInfo[0]?.lastOilChange).add(6, 'months').calendar()}</p>
         <TextField
           id="datetime-local"
           label="Next appointment"
@@ -84,7 +89,7 @@ const Maintance: React.FC<IMaintenanceProps> = (props) => {
       <div className="previousBrakeChange">
         <h2 className="card-title">Previous Brake Change:</h2>
         <p className="card-text">
-          Brake Pads Front:{carInfo[0] && carInfo[0].brakePadsFront}
+          Brake Pads Front:{carInfo[0]?.brakePadsFront}
         </p>
         <TextField
           id="datetime-local"
@@ -97,7 +102,7 @@ const Maintance: React.FC<IMaintenanceProps> = (props) => {
           }}
         />
         <p className="card-text">
-          Brake Pads Rear:{carInfo[0] && carInfo[0].brakePadRear}
+          Brake Pads Rear:{carInfo[0]?.brakePadRear}
         </p>
         <TextField
           id="datetime-local"
@@ -110,7 +115,7 @@ const Maintance: React.FC<IMaintenanceProps> = (props) => {
           }}
         />
         <p className="card-text">
-          Brake Routers Front:{carInfo[0] && carInfo[0].brakeRouterFront}
+          Brake Routers Front:{carInfo[0]?.brakeRouterFront}
         </p>
         <TextField
           id="datetime-local"
@@ -123,7 +128,7 @@ const Maintance: React.FC<IMaintenanceProps> = (props) => {
           }}
         />
         <p className="card-text">
-          Brake Routers Rear:{carInfo[0] && carInfo[0].brakeRouterRear}
+          Brake Routers Rear:{carInfo[0]?.brakeRouterRear}
         </p>
         <TextField
           id="datetime-local"
@@ -136,7 +141,7 @@ const Maintance: React.FC<IMaintenanceProps> = (props) => {
           }}
         />
         <p className="card-text">
-          Brake Calipers:{carInfo[0] && carInfo[0].BrakeCalipers}
+          Brake Calipers:{carInfo[0]?.BrakeCalipers}
         </p>
         <TextField
           id="datetime-local"
@@ -149,7 +154,7 @@ const Maintance: React.FC<IMaintenanceProps> = (props) => {
           }}
         />
         <p className="card-text">
-          Brake Drums:{carInfo[0] && carInfo[0].brakeDrum}
+          Brake Drums:{carInfo[0]?.brakeDrum}
         </p>
         <TextField
           id="datetime-local"
@@ -162,7 +167,7 @@ const Maintance: React.FC<IMaintenanceProps> = (props) => {
           }}
         />
         <p className="card-text">
-          Brake Hoses:{carInfo[0] && carInfo[0].brakeHoses}
+          Brake Hoses:{carInfo[0]?.brakeHoses}
         </p>
         <TextField
           id="datetime-local"
@@ -175,7 +180,7 @@ const Maintance: React.FC<IMaintenanceProps> = (props) => {
           }}
         />
         <p className="card-text">
-          Brake Line:{carInfo[0] && carInfo[0].brakeLines}
+          Brake Line:{carInfo[0]?.brakeLines}
         </p>
       </div>
       <div className="nextBrakeChange">
@@ -272,10 +277,10 @@ const Maintance: React.FC<IMaintenanceProps> = (props) => {
       <div className="previousBatterySwap">
         <h2 className="card-title">Previous Battery Swap:</h2>
         <p className="card-text">
-          Battery Size:{carInfo[0] && carInfo[0].batterySize}
+          Battery Size:{carInfo[0]?.batterySize}
         </p>
         <p className="card-text">Cold Cranking Amps:</p>
-        <p className="card-text">Date:{carInfo[0] && carInfo[0].batterySwap}</p>
+        <p className="card-text">Date:{carInfo[0]?.batterySwap}</p>
         <TextField
           id="datetime-local"
           label="Previous appointment"
@@ -306,7 +311,7 @@ const Maintance: React.FC<IMaintenanceProps> = (props) => {
       <div className="previousTireMaintenance">
         <h2 className="card-title">Previous Tire Maintenance</h2>
         <p className="card-text">
-          Tires Rotated:{carInfo[0] && carInfo[0].tireRotation}
+          Tires Rotated:{carInfo[0]?.tireRotation}
         </p>
         <TextField
           id="datetime-local"
@@ -330,10 +335,10 @@ const Maintance: React.FC<IMaintenanceProps> = (props) => {
           }}
         />
         <p className="card-text">
-          Tires Size:{carInfo[0] && carInfo[0].tireSize}
+          Tires Size:{carInfo[0]?.tireSize}
         </p>
         <p className="card-text">
-          Tires Air Pressure:{carInfo[0] && carInfo[0].tirePressure}
+          Tires Air Pressure:{carInfo[0]?.tirePressure}
         </p>
       </div>
       <div className="nextTireMaintenance">
