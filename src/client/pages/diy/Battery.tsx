cca914e6d64f8ad2381f75c43f32f8181ad43f3c
Jessica <jlatten.simon@gmail.com>
2020-08-07 14:42:52 -0500

    update to diy page

--- src/client/pages/diy/Battery.tsx
+++ src/client/pages/diy/Battery.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Container from 'react-bootstrap';
+import Container, { Jumbotron } from 'react-bootstrap';
 import jumbotron from 'react-bootstrap';
 import ReactPlayer from 'react-player'
 
@@ -11,29 +11,27 @@ export default class Battery extends React.Component {
   render() {
     return (
       <div className= "container">
-          <div className= "jumbotron row justify-content-center">
-          <h1>DIY Battery Maintenance</h1>
+          <div className= "jumbotron ">
+          <h1 className="card-title mb-4 mt-0">DIY Battery Maintenance</h1>
           </div>        
            
-           <div className= "d-flex justify-content-left row ">
-           <h3>Check Battery</h3></div>
-           <div className= "justify-content-left row 'player-wrapper' " >
+           <div className= "jumbotron">
+           <h3 className="card-title mb-4 mt-0">Check Battery</h3>
+           
            <ReactPlayer
-            className='react-player'
+            className='react-player' 
              url='https://www.youtube.com/watch?v=dfwhGd148DM'
-             width='100%'
-            />   
-          </div>
+             width='100%' />
+          </div> 
 
 
-          <div className= "d-flex justify-content-left row ">
-           <h3>Battery Replacement</h3></div>
-           <div className= "justify-content-left row 'player-wrapper' " >
+         <div className= "jumbotron  ">
+           <h3 className="card-title mb-4">Battery Replacement</h3>
+          
            <ReactPlayer
-            className='react-player'
+            className='react-player' 
              url='https://youtu.be/qz3upAdRuZs'
-             width='100%'
-            />   
+             width='100%' />   
           </div>
 
 
--- src/client/pages/diy/Fluids.tsx
+++ src/client/pages/diy/Fluids.tsx
@@ -11,13 +11,13 @@ export default class Fluids extends React.Component {
   render() {
     return (
       <div className= "container">
-          <div className= "jumbotron row justify-content-center">
-          <h1>DIY Fluid Maintenance</h1>
+          <div className= "jumbotron ">
+          <h1 className="card-title mb-4 mt-0">DIY Fluid Maintenance</h1>
           </div>        
            
-           <div className= "d-flex justify-content-left row ">
-           <h3>Check Coolant Levels</h3></div>
-           <div className= "justify-content-left row 'player-wrapper' " >
+           <div className= "jumbotron ">
+           <h3 className="card-title mb-4 mt-0">Check Coolant Levels</h3>
+           
            <ReactPlayer
             className='react-player'
              url='https://www.youtube.com/watch?v=SLwhD5uXceI'
@@ -26,9 +26,9 @@ export default class Fluids extends React.Component {
           </div>
 
 
-          <div className= "d-flex justify-content-left row ">
-           <h3>Transmission Fluid</h3></div>
-           <div className= "justify-content-left row 'player-wrapper' " >
+          <div className= "jumbotron ">
+          <h3 className="card-title mb-4">Transmission Fluid</h3>
+           
            <ReactPlayer
             className='react-player'
              url='https://www.youtube.com/watch?v=KlFrNZYl9oo'
@@ -36,9 +36,9 @@ export default class Fluids extends React.Component {
             />   
           </div>
 
-          <div className= "d-flex justify-content-left row ">
-           <h3>Cooling Fluid Exchange</h3></div>
-           <div className= "justify-content-left row 'player-wrapper' " >
+          <div className= "jumbotron  ">
+           <h3 className="card-title mb-4">Cooling Fluid Exchange</h3>
+           
            <ReactPlayer
             className='react-player'
              url='https://www.youtube.com/watch?v=g8YZF5cW7-A'
@@ -46,19 +46,15 @@ export default class Fluids extends React.Component {
             />   
           </div>
 
-          <div className= "d-flex justify-content-left row ">
-           <h3>Transfer Case Fluid</h3></div>
-           <div className= "justify-content-left row 'player-wrapper' " >
+          <div className= "jumbotron">
+          <h3 className="card-title mb-4">Transfer Case Fluid</h3>
+           
            <ReactPlayer
             className='react-player'
              url='https://www.youtube.com/watch?v=4Mz3nZ85ihQ'
              width='100%'
             />   
-          </div>
-
-
-
-          </div>
+                  </div></div>
           
           
 
